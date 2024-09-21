3012785080b450474c5eebd6e68000e6bebdb9b3
raze10339 <raze10339@yahoo.com>
2024-09-21 17:32:09 -0400

    finished arrays basics

--- js/js script.js
+++ js/js script.js
@@ -1,16 +1,34 @@
-const firstName = 'Razieh';
-let age = 12;
-                                       
-if (age>20 && age  <50) {
-console.log('you are young');
-}else if (age >= 50) {
-    console.log('ypu are getting older');
-}else if (age < 20 && age > 12) {
-    console.log(' you are teenager');
-}else {
-    console.log('your age is not within our range');
-}
+//arrays are not primitive value types
+//They are references types
+const fruits = ['apple', 'orange', 'kiwi', 'grape'];
+const names = ['roman', 'michael', 'nina', 'adonis'];
 
-const result = (0 || 75) && true && 'some sting' && true &&100;
-console.log(result);
+const data = ['JD', 'Tadlock', 44, '555 coding dr', '777-777-7777', ['pickleball', 'fishing']];
+
+console.log(fruits[2]);
+console.log(fruits.length);
+console.log(names[1]);
+console.log(data[5][1]);
+
+console.log(fruits);
+
+fruits[2] = 'strawberry';
+
+ console.log(fruits);
+
+ //Increase the data age number by one - console.log the original value and the new value after uou increase it
+
+ console.log(data[2]);
+ data[2] += 1;
+ console.log(data[2]);
+
+ let fullName = 'JD';
+
+ console.log(fullName);
+
+ fullName += 'Tadlock';
+
+ console.log(fullName);
+
+
 
