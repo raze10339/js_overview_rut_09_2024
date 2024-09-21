a6e3c6b795a9fcb3e698af1e015dae568a01e61c
raze10339 <raze10339@yahoo.com>
2024-09-21 18:05:59 -0400

    finished for loop basics

--- js/js script.js
+++ js/js script.js
@@ -1,34 +1,24 @@
 //arrays are not primitive value types
 //They are references types
-const fruits = ['apple', 'orange', 'kiwi', 'grape'];
+const fruits = ['apple', 'orange', 'kiwi', 'grape', 'banana', 'blueberry', 'strawberry', 'plantain'];
 const names = ['roman', 'michael', 'nina', 'adonis'];
 
 const data = ['JD', 'Tadlock', 44, '555 coding dr', '777-777-7777', ['pickleball', 'fishing']];
 
-console.log(fruits[2]);
-console.log(fruits.length);
-console.log(names[1]);
-console.log(data[5][1]);
+for(let index = 0; index< fruits.length; index++) {
+    console.log(fruits[index]);
+    }
 
-console.log(fruits);
+for(let count = 0; count< fruits.length; count++) {
+    console.log(fruits[count]);
+    }
 
-fruits[2] = 'strawberry';
 
- console.log(fruits);
 
- //Increase the data age number by one - console.log the original value and the new value after uou increase it
 
- console.log(data[2]);
- data[2] += 1;
- console.log(data[2]);
 
- let fullName = 'JD';
 
- console.log(fullName);
 
- fullName += 'Tadlock';
-
- console.log(fullName);
 
 
 
