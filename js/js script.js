91615c81ecf1aa79831a39c8f981db2dcd1a3748
raze10339 <raze10339@yahoo.com>
2024-09-22 19:36:53 -0400

    started web api

--- js/js script.js
+++ js/js script.js
@@ -1,57 +1,61 @@
-const user = {
-    name: 'JD',
-    age: 44,
-    phone: '777-777-7777',
-    address: '555 coding st',
-    info: {
-        location: 'Atlanta area',
-        hobbies: ['pickleball', 'fishing'],
-    },
-    printAge: function () {
-        console.log(this.age);
-    },
-    haveBirthday: function () {
-        // Increase the age of the user by one
-        this.age++;
-        // Print 'Happy Birthday' to the console
-        console.log('Happy Birthday!');
-    },
-    addHobby: function (hobby) {
-// need to reference an argument that is passed to addhobby [string of a hobby]
-console.log('it works!');
-// push the argument(hobby) to the info hobbies array
-this.info.hobbies.push(hobby)
-    },
-    printHobbies: function () {
-console.log(this.info.hobbies);
-    },
-};
-
-UserData.addHobby('tennis');
-UserData.addHobby('coding');
-UserData.addHobby('golf');
-UserData.addHobby('bingo');
-UserData.printHobbies();
-
-console.log('parent this', this);
-
-for (let i=0; i< this.info.hobbies.length; i++) {
-console.log(this.info.hobbies[i]);
+const header = document.querySelector('h1');
+const paragraph = document.querySelector('p:nth-child(2)');
+const image = document.querySelector('#main-image');
+const noteOutput = document.querySelector('.output');
+const timerDisplay = document.querySelector('#time');
+const timerBtn = document.querySelector('#start-btn');
 
 
-for (let hobby of this.info.hobbies) {
-    console.log(hobby);
+header.classList.add('crazy');
+
+header.style.textDecoration = "underline";
+
+//image.setAttribute('src', 'https://images.pexels.com/photos/16112572/pexels-photo-16112572/free-photo-of-view-of-sun-shining-between-the-trees-in-a-park.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2');
+
+//insert a new article element into the output section
+
+noteOutput.insertAdjacentHTML('beforeend', `
+    <article>
+        <h2>New note text</h2>
+        <p>Added On: 9/20/2024</p>
+    </article>
+`);
+
+//timers
+
+//setTimeout(function() {
+//console.log('time up!');  
+//}, 10000); 
+
+let count = 10;
+let started = false;
+
+
+
+
+
+
+timerBtn.addEventListener('click', function() {
+    if (!started) {
+    const timer = setInterval(function () {
+        count--;
+    
+        timerDisplay.innerText = 'Time: ' + count;
+    
+        if (count <= 0) {
+            clearInterval(timer);
+
+            timerDisplay.innerText = 'Time: 10';
+            count = 10;
+            started = false;
+        }
+    }, 1000);
+
+    started = true;
 }
-}
-//console.log(userData.addhobby),
 
-function test(hobby, age) {
-  console.log('hobby');
-}
-test();
-
-//userData.happyBirthday();
-//userData.printAge();
+started = false;
+});
 
 
 
